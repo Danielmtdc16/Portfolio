3f952875dc10d60b8e219a447dcccfc78168105b
Daniel <danielmtdc16@gmail.com>
2023-02-15 14:09:48 -0300

    Seção serviços concluída, cards exibindo conteúdo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,27 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalButtons = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalButtons.forEach((modalButton, i) => {
+    modalButton.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
