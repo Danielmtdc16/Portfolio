c86cf3dc387345508c2ce53407560df7de97d02c
Daniel <danielmtdc16@gmail.com>
2023-02-15 21:36:19 -0300

    Adicionado um carrossel para os projetos. Estilização desta etapa quase concluída.

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,18 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portifolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
